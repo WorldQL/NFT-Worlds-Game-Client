d8a20ea2ae5a527a85ff35ebdbecd32d984050db
Jack Baron <jackmbaron@gmail.com>
2022-06-09 12:24:48 +0100

    add navigation for testing

--- renderer/components/layout/Navbar.tsx
+++ renderer/components/layout/Navbar.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
 import { type FC } from 'react'
 
-export const Navbar: FC = () => <nav>aaa</nav>
+export const Navbar: FC = () => (
+  <nav>
+    <Link href='/'>Home</Link>
+    <Link href='/worlds'>Worlds</Link>
+    <Link href='/settings'>Settings</Link>
+  </nav>
+)
--- renderer/pages/worlds/index.tsx
+++ renderer/pages/worlds/index.tsx
@@ -2,6 +2,6 @@ import { type NextPage } from 'next'
 import React from 'react'
 import { Layout } from '~/components/layout/Layout'
 
-const Worlds: NextPage = () => <Layout>worlds list</Layout>
+const Worlds: NextPage = () => <Layout>Worlds List</Layout>
 
 export default Worlds
