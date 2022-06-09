60cae8b07b5f415e790aacae0ad94a1aab483504
Jack Baron <jackmbaron@gmail.com>
2022-06-09 08:02:31 +0100

    setup nextron

--- src/renderer/hooks/useStore.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-import { store } from '../state/store'
-
-export const useStore = () => {
-  const { state, dispatch } = useContext(store)
-  return { state, dispatch }
-}
