1463016481de9ec86e2364fb6295b86544ed08d1
Jack Baron <jackmbaron@gmail.com>
2022-06-13 15:05:15 +0100

    add blur to settings card

--- renderer/pages/settings.tsx
+++ renderer/pages/settings.tsx
@@ -6,13 +6,13 @@ import { TitleHeader } from '~/components/layout/TitleHeader'
 const Settings: NextPage = () => (
   <Layout>
     <TitleHeader>Settings</TitleHeader>
-    <div className='flex flex-col w-full flex-grow mb-[var(--pad)]'>
-      <div className='px-8 py-4 rounded-t-3xl flex gap-8 bg-blur-2 backdrop-blur-[var(--blur-amount)]'>
+    <div className='flex flex-col w-full flex-grow mb-[var(--pad)] bgblur rounded-3xl'>
+      <div className='px-8 py-4 rounded-t-3xl flex gap-8 bg-blur-1'>
         <span>Video</span>
         <span>Audio</span>
       </div>
 
-      <div className='rounded-b-3xl p-8 bg-blur-1 flex-grow'>settings</div>
+      <div className='rounded-b-3xl p-8 flex-grow'>settings</div>
     </div>
   </Layout>
 )
