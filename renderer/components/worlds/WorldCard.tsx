26cc6ced755c3250a7a5dd1727db00588310a842
Jack Baron <jackmbaron@gmail.com>
2022-06-13 16:43:09 +0100

    add ui transition to world card

--- renderer/components/worlds/WorldCard.tsx
+++ renderer/components/worlds/WorldCard.tsx
@@ -1,38 +1,49 @@
-import { type FC } from 'react'
+import { useRouter } from 'next/router'
+import { type FC, useCallback } from 'react'
 import { type World } from '~/lib/worlds'
 
 interface Props {
   world: World
 }
 
-export const WorldCard: FC<Props> = ({ world }) => (
-  <div className='bg-gradient-to-b from-bg-top to-bg-bottom rounded-2xl shadow-xl flex flex-col w-[var(--card-width)]'>
-    <img
-      className='h-[200px] object-cover rounded-t-2xl'
-      src={world.branding.banner}
-    />
+export const WorldCard: FC<Props> = ({ world }) => {
+  const { push } = useRouter()
+  const handleClick = useCallback(() => {
+    void push(`/worlds/${world.worldId}`)
+  }, [world.worldId, push])
 
-    <div className='rounded-b-2xl p-3 pt-0 pb-4 text-center'>
-      <div
-        className='aspect-square w-[4.5rem] h-auto rounded-full bg-cover border-2 shadow-[inset_0_0_8px_black] mt-[-2.25rem] mx-auto mb-2'
-        style={{ backgroundImage: `url(${world.branding.icon})` }}
+  return (
+    <div
+      className='bg-gradient-to-b from-bg-top to-bg-bottom rounded-2xl shadow-xl flex flex-col w-[var(--card-width)] cursor-pointer transition-transform duration-75 hover:-translate-y-1'
+      onClick={handleClick}
+    >
+      <img
+        className='h-[200px] object-cover rounded-t-2xl'
+        src={world.branding.banner}
       />
 
-      <div>
-        <h2 className='font-bold text-lg'>{world.name}</h2>
+      <div className='rounded-b-2xl p-3 pt-0 pb-4 text-center'>
+        <div
+          className='aspect-square w-[4.5rem] h-auto rounded-full bg-cover border-2 shadow-[inset_0_0_8px_black] mt-[-2.25rem] mx-auto mb-2'
+          style={{ backgroundImage: `url(${world.branding.icon})` }}
+        />
 
-        <span className='font-semibold text-xs text-text-sub'>
-          #{world.worldId} WORLD
-        </span>
+        <div>
+          <h2 className='font-bold text-lg'>{world.name}</h2>
 
-        <p className='text-xs text-text-sub line-clamp-2'>
-          {world.description}
-        </p>
+          <span className='font-semibold text-xs text-text-sub'>
+            #{world.worldId} WORLD
+          </span>
 
-        <span className='font-semibold text-xs text-text-sub'>
-          {world.playersOnline} PLAYERS ONLINE
-        </span>
+          <p className='text-xs text-text-sub line-clamp-2'>
+            {world.description}
+          </p>
+
+          <span className='font-semibold text-xs text-text-sub'>
+            {world.playersOnline} PLAYERS ONLINE
+          </span>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
