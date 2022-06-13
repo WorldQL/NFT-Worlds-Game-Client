b9b14cb86631611dfdcf478b72ce6a56330ee3c0
Jack Baron <jackmbaron@gmail.com>
2022-06-13 18:41:57 +0100

    implement dropdown UI

--- renderer/components/ui/Dropdown.tsx
+++ renderer/components/ui/Dropdown.tsx
@@ -1,31 +1,111 @@
-import { type ChangeEventHandler, type FC, useCallback } from 'react'
+import clsx from 'clsx'
+import {
+  type ChangeEventHandler,
+  type FC,
+  useCallback,
+  useMemo,
+  useState,
+} from 'react'
 
-interface Props {
-  options: readonly string[]
-  value: string
+export type DisplayHandler<T extends Record<string, string>> = (
+  value: keyof T,
+  options: T
+) => string
 
-  onChange: (value: string) => void
+interface Props<T extends Record<string, string>> {
+  options: T
+  value: keyof T
+
+  display?: DisplayHandler<T>
+  onChange: (value: keyof T) => void
 }
 
-export const Dropdown: FC<Props> = ({ options, value, onChange }) => {
-  const handleChange = useCallback<ChangeEventHandler<HTMLSelectElement>>(
-    ev => {
-      if (typeof onChange === 'function') onChange(ev.target.value)
+export const Dropdown = <T extends Record<string, string>>({
+  options,
+  value,
+  display,
+  onChange,
+}: Props<T>): ReturnType<FC> => {
+  const [open, setOpen] = useState<boolean>(false)
+
+  const transform = useCallback(
+    (value: keyof T) => {
+      if (typeof display === 'function') return display(value, options)
+
+      return options[value]
+    },
+    [options, display]
+  )
+
+  const handleMouseOver = useCallback(() => setOpen(true), [setOpen])
+  const handleMouseOut = useCallback(() => setOpen(false), [setOpen])
+
+  const handleClick = useCallback<Props<T>['onChange']>(
+    value => {
+      if (typeof onChange === 'function') onChange(value)
     },
     [onChange]
   )
 
+  const selected = useMemo<string>(() => transform(value), [value, transform])
   return (
-    <select
-      className='rounded-full bgblur px-3 py-4 w-[var(--card-width)]'
-      value={value}
-      onChange={handleChange}
+    <div className='w-[var(--card-width)] relative'>
+      <div
+        className={clsx(
+          'w-full bgblur px-6 py-4 cursor-pointer',
+          !open && 'rounded-full',
+          open &&
+            'rounded-t-[28px] rounded-b-none shadow-none !backdrop-blur-[var(--blur-amount-fix)]'
+        )}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        {selected}
+      </div>
+
+      {open && (
+        <div
+          className='absolute w-full bgblur flex flex-col gap-2 rounded-b-[28px]'
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          {Object.keys(options).map(key => (
+            <DropdownItem
+              key={key}
+              value={key}
+              display={transform(key)}
+              onClick={handleClick}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+interface ItemProps<T extends Record<string, string>> {
+  value: keyof T
+  display: string
+
+  onClick: (value: keyof T) => void
+}
+
+export const DropdownItem = <T extends Record<string, string>>({
+  value,
+  display,
+  onClick,
+}: ItemProps<T>): ReturnType<FC> => {
+  const handleClick = useCallback(() => {
+    if (typeof onClick === 'function') onClick(value)
+  }, [value, onClick])
+
+  return (
+    <div
+      className='px-6 py-3 hover:bg-blur-2 bg-opacity-50 first:mt-2 last:rounded-b-[28px] last:pb-4 cursor-pointer'
+      onClick={handleClick}
     >
-      {options.map(option => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+      {display}
+      {/* TODO: Add checkbox icon */}
+    </div>
   )
 }
--- renderer/pages/worlds/index.tsx
+++ renderer/pages/worlds/index.tsx
@@ -2,31 +2,51 @@ import { type NextPage } from 'next'
 import React, { useEffect, useState } from 'react'
 import { Layout } from '~/components/layout/Layout'
 import { TitleHeader } from '~/components/layout/TitleHeader'
-import { Dropdown } from '~/components/ui/Dropdown'
+import { type DisplayHandler, Dropdown } from '~/components/ui/Dropdown'
 import { Searchbar } from '~/components/ui/Searchbar'
 import { WorldGrid } from '~/components/worlds/WorldGrid'
 import { fetchWorlds, type World } from '~/lib/worlds'
 
+// TODO: Hook up to global state
+const worldFilter = {
+  all: 'All Worlds',
+  singleplayer: 'Single Player',
+  multiplayer: 'Multiplayer',
+  firstPerson: 'First Person',
+  thirdPerson: 'Third Person',
+} as const
+
+// TODO: Hook up to global state
+const worldSort = {
+  recentUpdate: 'Recently Updated',
+  // TODO: More sort options
+} as const
+
+const displaySort: DisplayHandler<typeof worldSort> = (value, options) =>
+  `Sort: ${options[value]}`
+
 const Worlds: NextPage = () => {
+  // TODO: Make global
   const [worlds, setWorlds] = useState<readonly World[] | undefined>(undefined)
   useEffect(() => {
     void fetchWorlds().then(worlds => setWorlds(worlds))
   }, [])
+
+  // TODO: Hook up to global state
+  const [filter, setFilter] = useState<keyof typeof worldFilter>('all')
+  const [sort, setSort] = useState<keyof typeof worldSort>('recentUpdate')
 
   return (
     <Layout>
       <TitleHeader>Worlds</TitleHeader>
 
       <div className='mb-8 grid grid-cols-[repeat(5,var(--card-width))] justify-between'>
-        <Dropdown
-          value=''
-          options={['All Worlds']}
-          onChange={() => undefined}
-        />
+        <Dropdown options={worldFilter} value={filter} onChange={setFilter} />
 
         <Dropdown
-          value=''
-          options={['Sort: Recently Updated']}
+          value='recentUpdate'
+          options={worldSort}
+          display={displaySort}
           onChange={() => undefined}
         />
 
