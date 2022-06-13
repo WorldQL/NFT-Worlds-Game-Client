25fdc6547b442934462b1f0390cde0a311cba9a3
Jack Baron <jackmbaron@gmail.com>
2022-06-13 13:44:56 +0100

    adjust world card rounding

--- renderer/components/worlds/WorldCard.tsx
+++ renderer/components/worlds/WorldCard.tsx
@@ -6,13 +6,13 @@ interface Props {
 }
 
 export const WorldCard: FC<Props> = ({ world }) => (
-  <div className='bg-gradient-to-b from-bg-top to-bg-bottom rounded-lg shadow-xl flex flex-col w-[var(--card-width)]'>
+  <div className='bg-gradient-to-b from-bg-top to-bg-bottom rounded-2xl shadow-xl flex flex-col w-[var(--card-width)]'>
     <img
-      className='h-[200px] object-cover rounded-t-lg'
+      className='h-[200px] object-cover rounded-t-2xl'
       src={world.branding.banner}
     />
 
-    <div className='rounded-b-lg p-3 pt-0 pb-4 text-center'>
+    <div className='rounded-b-2xl p-3 pt-0 pb-4 text-center'>
       <div
         className='aspect-square w-[4.5rem] h-auto rounded-full bg-cover border-2 shadow-[inset_0_0_8px_black] mt-[-2.25rem] mx-auto mb-2'
         style={{ backgroundImage: `url(${world.branding.icon})` }}
