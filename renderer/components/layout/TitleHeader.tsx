068fa33d47ce5124219023e45e8788c73bb0614c
Jack Baron <jackmbaron@gmail.com>
2022-06-13 13:25:09 +0100

    adjust title header spacing

--- renderer/components/layout/TitleHeader.tsx
+++ renderer/components/layout/TitleHeader.tsx
@@ -1,5 +1,5 @@
 import { type FC, type PropsWithChildren } from 'react'
 
 export const TitleHeader: FC<PropsWithChildren> = ({ children }) => (
-  <h1 className='text-center text-[4.8rem] font-bold my-6'>{children}</h1>
+  <h1 className='text-center text-[4.8rem] font-bold mt-5 mb-7'>{children}</h1>
 )
