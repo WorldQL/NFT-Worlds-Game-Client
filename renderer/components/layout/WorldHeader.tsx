880f55d6e6fc0706ac97ca6994aadbf35bcf9f63
Jack Baron <jackmbaron@gmail.com>
2022-06-13 13:45:04 +0100

    add random future components

---
+++ renderer/components/layout/WorldHeader.tsx
@@ -0,0 +1,8 @@
+import { type FC } from 'react'
+import { type World } from '~/lib/worlds'
+
+interface Props {
+  world: World
+}
+
+export const WorldHeader: FC<Props> = ({ world }) => null
---
+++ renderer/components/ui/ButtonSecondary.tsx
@@ -0,0 +1,21 @@
+import { type FC, type PropsWithChildren, useCallback } from 'react'
+
+interface Props {
+  onClick: () => void
+}
+
+export const ButtonSecondary: FC<PropsWithChildren<Props>> = ({
+  children,
+  onClick,
+}) => {
+  const handleClick = useCallback(() => {
+    if (typeof onClick === 'function') onClick()
+  }, [onClick])
+
+  // TODO: Styling
+  return (
+    <button type='button' onClick={handleClick}>
+      {children}
+    </button>
+  )
+}
