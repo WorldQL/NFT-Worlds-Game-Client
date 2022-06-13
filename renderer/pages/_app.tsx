4e846e183b9a3e2a945f64fb3c6297c6f11364da
Jack Baron <jackmbaron@gmail.com>
2022-06-13 14:45:24 +0100

    re-add konami code devtools toggle

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from 'next/app'
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useKonami } from 'react-konami-code'
 import { Titlebar } from '~/components/layout/Titlebar'
+
 import '~/styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const handleKonami = useCallback(async () => {
+    const { getCurrentWebContents } = await import('@electron/remote')
+    const webContents = getCurrentWebContents()
+
+    webContents.toggleDevTools()
+  }, [])
+
+  useKonami(handleKonami)
+
   return (
     <div className='w-screen h-screen flex flex-col rounded-window overflow-hidden'>
       <Titlebar />
