bb3f039cb7c236dabf8eab90f61c4e00adb20824
Jack Baron <jackmbaron@gmail.com>
2022-06-13 14:22:09 +0100

    touch up navbar

--- renderer/components/layout/Navbar.tsx
+++ renderer/components/layout/Navbar.tsx
@@ -1,14 +1,21 @@
 import Link from 'next/link'
 import { type FC } from 'react'
+import Logo from '~/assets/svg/logo.svg'
 
 export const Navbar: FC = () => (
-  <nav className='min-h-[4rem] mt-8 py-2 px-5 flex items-center gap-8 bgblur rounded-full text-xl'>
-    <div className='flex-grow'>&lt;logo&gt;</div>
+  <nav className='[--dropdown-width:16rem] min-h-[4rem] mt-8 flex items-center bgblur rounded-full text-xl'>
+    <div className='w-[var(--dropdown-width)] pl-5'>
+      <img className='h-9' src={Logo.src} />
+    </div>
 
-    <Link href='/'>Home</Link>
-    <Link href='/worlds'>Worlds</Link>
-    <Link href='/settings'>Settings</Link>
+    <div className='flex-grow flex gap-10 justify-center'>
+      <Link href='/'>Home</Link>
+      <Link href='/worlds'>Worlds</Link>
+      <Link href='/settings'>Settings</Link>
+    </div>
 
-    <div className='flex-grow text-right'>&lt;user dropdown&gt;</div>
+    <div className='h-full w-[var(--dropdown-width)] rounded-r-full bg-blur-1 flex items-center justify-center'>
+      &lt;user dropdown&gt;
+    </div>
   </nav>
 )
---
+++ renderer/env.d.ts
@@ -0,0 +1,5 @@
+declare module '~/assets/svg/*.svg' {
+  const content: import('next/image').StaticImageData
+
+  export default content
+}
