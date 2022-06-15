dd8ed085ed6012f3c52aa767d7308124274e3821
Jack Baron <jackmbaron@gmail.com>
2022-06-15 16:27:01 +0100

    properly style search bar

--- renderer/components/ui/Searchbar.tsx
+++ renderer/components/ui/Searchbar.tsx
@@ -1,4 +1,5 @@
-import { type ChangeEventHandler, type FC, useCallback } from 'react'
+import { type ChangeEventHandler, type FC, useCallback, useRef } from 'react'
+import { Search } from '~/components/svg/Search'
 
 interface Props {
   value: string
@@ -8,6 +9,7 @@ interface Props {
 }
 
 export const Searchbar: FC<Props> = ({ value, placeholder, onChange }) => {
+  const ref = useRef<HTMLInputElement>(null)
   const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
     ev => {
       if (typeof onChange === 'function') onChange(ev.target.value)
@@ -15,13 +17,25 @@ export const Searchbar: FC<Props> = ({ value, placeholder, onChange }) => {
     [onChange]
   )
 
+  const focus = useCallback(() => {
+    ref.current?.focus()
+  }, [])
+
   return (
-    <input
-      className='rounded-full bgblur backdrop-filter-[50px] px-3 py-4 w-[400px]'
-      type='text'
-      value={value}
-      placeholder={placeholder}
-      onChange={handleChange}
-    />
+    <div
+      className='rounded-full bgblur backdrop-filter-[50px] w-[400px] flex gap-4 items-center cursor-text'
+      onClick={focus}
+    >
+      <input
+        ref={ref}
+        type='text'
+        className='px-6 py-4 rounded-l-full grow bg-transparent outline-none'
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+
+      <Search className='mr-6 pointer-events-none' />
+    </div>
   )
 }
