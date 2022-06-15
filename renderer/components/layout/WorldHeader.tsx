0bc0f4c83583cba80a3ea6864b0f90e67ce1715e
Jack Baron <jackmbaron@gmail.com>
2022-06-15 14:25:36 +0100

    implement world header

--- renderer/components/layout/WorldHeader.tsx
+++ renderer/components/layout/WorldHeader.tsx
@@ -1,8 +1,50 @@
-import { type FC } from 'react'
+import { type FC, useCallback } from 'react'
+import { ButtonPrimary } from '~/components/ui/ButtonPrimary'
+import { ButtonSecondary } from '~/components/ui/ButtonSecondary'
+import { PlayersOnline } from '~/components/worlds/PlayersOnline'
 import { type World } from '~/lib/data/worlds'
 
 interface Props {
   world: World
 }
 
-export const WorldHeader: FC<Props> = ({ world }) => null
+export const WorldHeader: FC<Props> = ({ world }) => {
+  const handleClickPrimary = useCallback(() => {
+    // TODO
+  }, [])
+
+  const handleClickSecondary = useCallback(() => {
+    // TODO
+  }, [])
+
+  return (
+    <div className='flex flex-col mx-[var(--card-width)] mt-8'>
+      <div className='flex items-center gap-5'>
+        <div
+          className='aspect-square w-[4.5rem] h-auto rounded-full bg-cover border-2 shadow-[inset_0_0_8px_black]'
+          style={{ backgroundImage: `url(${world.branding.icon})` }}
+        />
+
+        <span className='text-sm'>#{world.worldId} WORLD</span>
+      </div>
+
+      <h1 className='text-[4.8rem] font-bold leading-none mt-2'>
+        {world.name}
+      </h1>
+
+      <p className='mt-5 line-clamp-1'>{world.description}</p>
+
+      <div className='flex gap-3 mt-6'>
+        <div className='flex flex-col gap-3 items-center'>
+          <ButtonPrimary onClick={handleClickPrimary}>Play Now</ButtonPrimary>
+
+          <PlayersOnline world={world} />
+        </div>
+
+        <ButtonSecondary onClick={handleClickSecondary}>
+          Secondary Button
+        </ButtonSecondary>
+      </div>
+    </div>
+  )
+}
--- renderer/pages/worlds/[world].tsx
+++ renderer/pages/worlds/[world].tsx
@@ -2,12 +2,23 @@ import { type NextPage } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
 import { Layout } from '~/components/layout/Layout'
+import { WorldHeader } from '~/components/layout/WorldHeader'
+import { useWorlds } from '~/lib/hooks/useWorlds'
 
 const World: NextPage = () => {
   const { query } = useRouter()
-  const world = query.world
+  const { worlds } = useWorlds()
 
-  return <Layout>World: {world}</Layout>
+  const worldId = Number.parseInt(query.world! as string, 10)
+  const world = worlds?.find(x => x.worldId === worldId)
+
+  if (!world) return null
+
+  return (
+    <Layout>
+      <WorldHeader world={world} />
+    </Layout>
+  )
 }
 
 export default World
