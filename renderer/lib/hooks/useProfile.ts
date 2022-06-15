b5c582f86ee16882fd33a4ffe4417855d07156b2
Jack Baron <jackmbaron@gmail.com>
2022-06-15 14:28:38 +0100

    refresh profile info every 30s

--- renderer/lib/hooks/useProfile.ts
+++ renderer/lib/hooks/useProfile.ts
@@ -1,11 +1,15 @@
 import { type AxiosError } from 'axios'
+import ms from 'ms'
 import useSWR from 'swr'
 import { getProfile, type Profile } from '~/lib/data/profile'
 
 export const useProfile = () => {
   const { data: profile, error } = useSWR<Profile, AxiosError>(
     '/profile',
-    getProfile
+    getProfile,
+    {
+      refreshInterval: ms('30s'),
+    }
   )
 
   const loading = !profile && !error
