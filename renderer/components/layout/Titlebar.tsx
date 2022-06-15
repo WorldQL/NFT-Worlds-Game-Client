5a1535233d1b1d8d9fc18c5e74730024bcc03d88
Jack Baron <jackmbaron@gmail.com>
2022-06-15 16:05:26 +0100

    extract svg components

--- renderer/components/layout/Titlebar.tsx
+++ renderer/components/layout/Titlebar.tsx
@@ -1,5 +1,7 @@
 import clsx from 'clsx'
 import { type FC, type PropsWithChildren, useCallback } from 'react'
+import { Close } from '~/components/svg/Close'
+import { Minimise } from '~/components/svg/Minimise'
 
 export const Titlebar: FC = () => {
   const handleMinimise = useCallback(async () => {
@@ -51,20 +53,4 @@ const Icon: FC<PropsWithChildren<IconProps>> = ({
   >
     {children}
   </div>
-)
-
-const Close: FC = () => (
-  <svg aria-hidden='false' width='12' height='12' viewBox='0 0 12 12'>
-    <polygon
-      fill='currentColor'
-      fillRule='evenodd'
-      points='11 1.576 6.583 6 11 10.424 10.424 11 6 6.583 1.576 11 1 10.424 5.417 6 1 1.576 1.576 1 6 5.417 10.424 1'
-    />
-  </svg>
-)
-
-const Minimise: FC = () => (
-  <svg aria-hidden='false' width='12' height='12' viewBox='0 0 12 12'>
-    <rect fill='currentColor' width='10' height='1' x='1' y='6' />
-  </svg>
 )
---
+++ renderer/components/svg/Minimise.tsx
@@ -0,0 +1,7 @@
+import { type FC } from 'react'
+
+export const Minimise: FC = () => (
+  <svg aria-hidden='false' width='12' height='12' viewBox='0 0 12 12'>
+    <rect fill='currentColor' width='10' height='1' x='1' y='6' />
+  </svg>
+)
