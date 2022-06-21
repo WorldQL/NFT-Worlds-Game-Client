216a69b7a56bbbca2aee9d4573a03cac34a65590
Jack Baron <jackmbaron@gmail.com>
2022-06-21 15:42:52 +0100

    add transition delay to carousel

--- renderer/components/layout/WorldHeaderCarousel.tsx
+++ renderer/components/layout/WorldHeaderCarousel.tsx
@@ -1,4 +1,5 @@
-import { type FC, type Reducer, useMemo, useReducer } from 'react'
+import clsx from 'clsx'
+import { type FC, type Reducer, useEffect, useMemo, useReducer } from 'react'
 import { WorldHeader } from '~/components/layout/WorldHeader'
 import { useWorlds } from '~/lib/hooks/useWorlds'
 
@@ -6,47 +7,99 @@ interface Props {
   count?: number
 }
 
+interface State {
+  index: number
+  transition: 'left' | 'right' | undefined
+}
+
 type Action =
   | { action: 'increment' }
   | { action: 'decrement' }
-  | { action: 'set'; value: number }
+  | { action: 'setIdx'; value: number }
+  | { action: 'end' }
 
 export const WorldHeaderCarousel: FC<Props> = ({ count = 3 }) => {
   const { worlds: allWorlds } = useWorlds()
   const worlds = useMemo(() => allWorlds?.slice(0, count), [allWorlds, count])
 
-  const [index, setIndex] = useReducer<Reducer<number, Action>>(
+  const [state, dispatch] = useReducer<Reducer<State, Action>>(
     (previousState, action) => {
       switch (action.action) {
         case 'increment': {
-          return (previousState + 1) % count
+          if (previousState.transition !== undefined) {
+            return previousState
+          }
+
+          const index = (previousState.index + 1) % count
+          const transition = 'right'
+
+          return { ...previousState, index, transition }
         }
 
         case 'decrement': {
-          return (previousState + count - 1) % count
+          if (previousState.transition !== undefined) {
+            return previousState
+          }
+
+          const index = (previousState.index + count - 1) % count
+          const transition = 'left'
+
+          return { ...previousState, index, transition }
         }
 
-        case 'set': {
-          return action.value % count
+        case 'setIdx': {
+          if (previousState.transition !== undefined) {
+            return previousState
+          }
+
+          const index = action.value % count
+          // TODO: Calculate Transition
+
+          return { ...previousState, index }
+        }
+
+        case 'end': {
+          return { ...previousState, transition: undefined }
         }
 
         default:
           throw new Error('invalid action')
       }
     },
-    0
+    { index: 0, transition: undefined }
   )
 
-  if (worlds === undefined) return null
-  return (
-    <div>
-      <WorldHeader world={worlds[index]} secondary='details' />
+  const { index, transition } = state
+  useEffect(() => {
+    if (transition === undefined) return
+    const timeout = setTimeout(() => dispatch({ action: 'end' }), 1000)
 
-      <button type='button' onClick={() => setIndex({ action: 'increment' })}>
+    return () => clearTimeout(timeout)
+  }, [transition])
+
+  if (worlds === undefined) return null
+
+  return (
+    <div className='relative'>
+      <WorldHeader
+        world={worlds[index]}
+        secondary='details'
+        className={clsx('transition-transform')}
+      />
+
+      <WorldHeader
+        world={worlds[index]}
+        secondary='details'
+        className={clsx('transition-transform absolute top-0')}
+      />
+
+      <pre>{JSON.stringify({ transition })}</pre>
+
+      <button type='button' onClick={() => dispatch({ action: 'increment' })}>
         Increment
       </button>
 
-      <button type='button' onClick={() => setIndex({ action: 'decrement' })}>
+      <button type='button' onClick={() => dispatch({ action: 'decrement' })}>
         Decrement
       </button>
     </div>
