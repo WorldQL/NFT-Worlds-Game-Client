628138ef9f1d4ae01f199356c2a01d07f3f40e46
Jack Baron <jackmbaron@gmail.com>
2022-06-27 14:16:28 +0100

    always take up 1/2 width

--- renderer/components/settings/SettingsColumn.tsx
+++ renderer/components/settings/SettingsColumn.tsx
@@ -1,5 +1,5 @@
 import { type FC, type PropsWithChildren } from 'react'
 
 export const SettingsColumn: FC<PropsWithChildren> = ({ children }) => (
-  <div className='grow p-6 h-full flex flex-col'>{children}</div>
+  <div className='w-1/2 p-6 h-full flex flex-col'>{children}</div>
 )
