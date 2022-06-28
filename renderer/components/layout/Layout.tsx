dd891f4c3b5de44071f9152fb06305e9820fd631
Jack Baron <jackmbaron@gmail.com>
2022-06-28 13:03:31 +0100

    fix flicker

--- renderer/components/layout/Layout.tsx
+++ renderer/components/layout/Layout.tsx
@@ -1,35 +1,44 @@
 import clsx from 'clsx'
 import { type FC, type PropsWithChildren, useEffect, useRef } from 'react'
+import { createGlobalState } from 'react-hooks-global-state'
 import { Navbar } from './Navbar'
 
 interface Props {
   scroll?: boolean
 }
 
+interface GlobalState {
+  pad: number
+}
+
+const initialState: GlobalState = {
+  pad: 0,
+}
+
+const { useGlobalState } = createGlobalState(initialState)
+export { useGlobalState }
+
 export const Layout: FC<PropsWithChildren<Props>> = ({ scroll, children }) => {
   const navRef = useRef<HTMLElement>(null)
-  const contentRef = useRef<HTMLDivElement>(null)
+  const [pad, setPad] = useGlobalState('pad')
 
   useEffect(() => {
     const nav = navRef.current
-    const content = contentRef.current
-
     if (!nav) return
-    if (!content) return
 
     const style = getComputedStyle(nav)
     const height = nav.offsetHeight
     const margin = Number.parseInt(style.marginTop, 10)
 
-    content.style.paddingTop = `${height + margin}px`
-  }, [])
+    setPad(height + margin)
+  }, [setPad])
 
   return (
     <div className='relative h-full flex flex-col mx-[var(--pad)]'>
       <Navbar ref={navRef} />
 
       <div
-        ref={contentRef}
+        style={{ paddingTop: `${pad}px` }}
         className={clsx(
           'flex-1 flex flex-col',
           !scroll && 'overflow-y-clip',
