911c04b6629149cde91b8a7d2f15245053832d78
Jack Baron <jackmbaron@gmail.com>
2022-07-08 18:41:48 +0100

    fix imports

--- renderer/pages/inventory.tsx
+++ renderer/pages/inventory.tsx
@@ -1,8 +1,8 @@
 import { type NextPage } from 'next'
-import { Loadout } from '~/components/layout/inventory/Loadout'
-import { LoadoutContainer } from '~/components/layout/inventory/LoadoutContainer'
-import { LoadoutItems } from '~/components/layout/inventory/LoadoutItems'
-import { SkinPreview } from '~/components/layout/inventory/SkinPreview'
+import { Loadout } from '~/components/inventory/Loadout'
+import { LoadoutContainer } from '~/components/inventory/LoadoutContainer'
+import { LoadoutItems } from '~/components/inventory/LoadoutItems'
+import { SkinPreview } from '~/components/inventory/SkinPreview'
 import { Layout } from '~/components/layout/Layout'
 
 const Inventory: NextPage = () => (
