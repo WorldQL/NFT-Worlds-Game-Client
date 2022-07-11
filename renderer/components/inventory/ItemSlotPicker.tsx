9017a1dd245ca742ec035f010874cb733bda06c1
Jack Baron <jackmbaron@gmail.com>
2022-07-11 12:58:42 +0100

    add dropdown to item slot picker

--- renderer/components/inventory/ItemSlotPicker.tsx
+++ renderer/components/inventory/ItemSlotPicker.tsx
@@ -1,4 +1,26 @@
-import { type FC } from 'react'
+import { type FC, useState } from 'react'
+import { Card, CardBody } from '~/components/layout/Card'
+import { Dropdown } from '~/components/ui/Dropdown'
+
+// TODO: Hook up to global state
+const itemsFilter = {
+  all: 'All Heads',
+} as const
 
 // TODO
-export const ItemSlotPicker: FC = () => <div className='w-[28%] ' />
+export const ItemSlotPicker: FC = () => {
+  const [filter, setFilter] = useState<keyof typeof itemsFilter>('all')
+
+  return (
+    <Card className='w-[28%]'>
+      <CardBody noTitle className='flex flex-col items-center p-4'>
+        <Dropdown
+          options={itemsFilter}
+          value={filter}
+          className='!w-full'
+          onChange={setFilter}
+        />
+      </CardBody>
+    </Card>
+  )
+}
